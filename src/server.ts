7ad3f53cd3eeb7902e2fc1dd173d3a1a8207dbc0
Richard Ng <richardcrng@gmail.com>
2021-11-16 17:04:05 +0000

    Set up CORS middleware

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import {
   addDummyDbItems,
@@ -15,8 +16,11 @@ import filePath from "./filePath";
 addDummyDbItems(20);
 
 const app = express();
+
 /** Parses JSON data in a request automatically */
 app.use(express.json());
+/** To allow 'Cross-Origin Resource Sharing': https://en.wikipedia.org/wiki/Cross-origin_resource_sharing */
+app.use(cors());
 
 // read in contents of any environment variables in the .env file
 dotenv.config();
